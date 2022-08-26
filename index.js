82cc69ee53f4906c51792da3eb9a9d87abe3fa62
shinar3@gmail.com <shenarfichman@Siddeeqahs-MacBook-Pro.local>
2022-08-25 20:22:45 -0400

    commit tree b7a9d3b98769c54a653bb67acc7fa8781eeaaef1

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution in this file!
+const employee = {
+    name: {
+        first: "Shenar",
+        last: "Fichman",
+     
+    },
+    streetAddress: "4105 belle ave"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+  
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+
+  //  const obj = obj.splice(key);
+
+    obj[key] = value;
+  
+    return obj;
+
+
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+   delete employee[key] 
+
+    return employee
+
+};
+function deleteFromEmployeeByKey(obj, key, value){
+
+
+const newObj = { ...obj };
+
+    newObj[key] = value;
+  
+    return newObj;
+};
